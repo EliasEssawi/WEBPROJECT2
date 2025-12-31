979536018735b8bc9efc1e6d0a1fac4801af2f37
EliasEssawi <elias.essawi@e.braude.ac.il>
2025-12-31 17:40:21 +0200

    LASTCOMMY

--- client/vite-project/src/main.tsx
+++ client/vite-project/src/main.tsx
@@ -3,10 +3,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import App from "./App.tsx";
-import AppTRAIN from "./AppTRAIN.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppTRAIN />
+    <App/>
   </StrictMode>
 );
